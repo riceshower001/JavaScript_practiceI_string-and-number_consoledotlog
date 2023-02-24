8a24420a978d32c4d1d08cdf0af8de778c70f7ce
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 03:54:23 +0900

    Add files via upload

---
+++ JavaScript_practiceI_string and number_consoledotlog.js
@@ -0,0 +1,8 @@
+// Please print "Hello World" to the console.
+console.log("Hello World");
+
+// Please print "*** ***" to the console.
+console.log("*** ***");
+
+// Please comment out the following lines.
+// console.log("◆◆◆ ◆◆◆");
